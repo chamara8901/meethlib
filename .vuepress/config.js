a9898fc82224853c06c2de4ace8e0fa1aaad7bae
chamara8901 <chamara@grrow.jp>
2022-08-04 15:31:22 +0900

    new

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: 'meeth',
+    description:'meeth documents',
+
+    themeConfig: {
+        nav:[
+            {text:'Home', link:'/'},
+            {text:'Documents', link:'/documents/'}
+        ],
+        sidebar:{
+            '/documents/':[
+                '',
+                '_Sidebar',
+            ]
+        }
+
+    }
+}
