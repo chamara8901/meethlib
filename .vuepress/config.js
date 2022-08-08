33b5b96ad5481a573f7e81ff944f8e77802b1e4a
chamara8901 <chamara@grrow.jp>
2022-08-08 17:51:58 +0900

    new

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -7,29 +7,29 @@ module.exports = {
             {text:'Home', link:'/'},
             {text:'Documents', link:'/documents/'}
         ],
-        sidebar:{
-            '/documents/':[
-                '',
-                '_Sidebar',
-                '[環境情報]-DB接続情報',
-                '「WIP」GMO実売上化',
-                '01_ジェネラル',
-                '01-仕様・要件整理',
-                '02-機能要件',
-                '03_開発',
-                '04_UIUXデザイン-アセット',
-                '04_プログラム',
-                '05_テスト',
-                '06_運用',
-                '07_Others',
-                'basic-specification-customer_import',
-                'basic-specification-order_import',
-                'basic-specification-order_import',
-                'DB初期化スクリプト',
-                'GMO仕様',
-                'Home'
-            ]
-        }
+        // sidebar:{
+        //     '/documents/':[
+        //         '',
+        //         '_Sidebar',
+        //         '[環境情報]-DB接続情報',
+        //         '「WIP」GMO実売上化',
+        //         '01_ジェネラル',
+        //         '01-仕様・要件整理',
+        //         '02-機能要件',
+        //         '03_開発',
+        //         '04_UIUXデザイン-アセット',
+        //         '04_プログラム',
+        //         '05_テスト',
+        //         '06_運用',
+        //         '07_Others',
+        //         'basic-specification-customer_import',
+        //         'basic-specification-order_import',
+        //         'basic-specification-order_import',
+        //         'DB初期化スクリプト',
+        //         'GMO仕様',
+        //         'Home'
+        //     ]
+        // }
 
     }
 }
